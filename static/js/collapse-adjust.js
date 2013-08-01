a5fa0b27b535f8b77a963cd37c934e6e2f667428
nmorduch <nmorduch@oberlin.edu>
2013-07-31 23:37:55 -0400

    updated collpase-adjust to work, and without being so tied to how the html is formed

--- static/js/collapse-adjust.js
+++ static/js/collapse-adjust.js
@@ -1,14 +1,14 @@
 $(function(){
 	$(document.body).on("show", ".collapse", function (event) {
 		var $this = $(this);
-		if ($this.prev().children(".collapse-handle").size() > 0) {
-			$this.prev().children(".collapse-handle").removeClass("icon-chevron-down").addClass("icon-remove");
+		if ($this.prev().find(".collapse-handle").size() > 0) {
+			$this.prev().find(".collapse-handle").removeClass("icon-chevron-down").addClass("icon-remove");
 		}
 	});
 	$(document.body).on("hide", ".collapse", function (event) {
 		var $this = $(this);
-		if ($this.prev().children(".collapse-handle").size() > 0) {
-			$this.prev().children(".collapse-handle").removeClass("icon-remove").addClass("icon-chevron-down");
+		if ($this.prev().find(".collapse-handle").size() > 0) {
+			$this.prev().find(".collapse-handle").removeClass("icon-remove").addClass("icon-chevron-down");
 		}
 	});
 });
