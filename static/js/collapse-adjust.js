dd9687528c11226e09246d7101784506fac7c466
nmorduch <nmorduch@oberlin.edu>
2014-03-11 23:10:52 -0400

    colors and collapse-adjust fix

--- static/js/collapse-adjust.js
+++ static/js/collapse-adjust.js
@@ -2,13 +2,13 @@ $(function(){
 	$(document.body).on("show", ".collapse", function (event) {
 		var $this = $(this);
 		if ($this.prev().find(".collapse-handle").size() > 0) {
-			$this.prev().find(".collapse-handle").removeClass("icon-chevron-down").addClass("icon-remove");
+			$this.prev().find(".collapse-handle").removeClass("fa-chevron-down").addClass("fa-times");
 		}
 	});
 	$(document.body).on("hide", ".collapse", function (event) {
 		var $this = $(this);
 		if ($this.prev().find(".collapse-handle").size() > 0) {
-			$this.prev().find(".collapse-handle").removeClass("icon-remove").addClass("icon-chevron-down");
+			$this.prev().find(".collapse-handle").removeClass("fa-times").addClass("fa-chevron-down");
 		}
 	});
 });
